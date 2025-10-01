f7e130a2480f2536b4e71bd9b46ea631cc2ba424
zachnewman4 <zachnewman04@gmail.com>
2025-10-01 20:23:42 +0100

    config added, smoketest works

--- src/utils/io.py
+++ src/utils/io.py
@@ -2,6 +2,8 @@ import yaml
 from pathlib import Path
 
 def load_config(path: str):
-    path = Path(path)
-    with open(path, "r") as f:
+    # Resolve relative to project root
+    root = Path(__file__).resolve().parents[2]  # go up from src/utils/io.py → Project2/
+    full_path = root / path
+    with open(full_path, "r") as f:
         return yaml.safe_load(f)
